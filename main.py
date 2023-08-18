69183d5a6811a58fd6949867ca76de5914bf6413
ISeeBugsEverywhere <menulio.svytuokle@gmail.com>
2023-08-18 17:33:05 +0300

    Init

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ class OrielWindow(QtWidgets.QMainWindow):
         bts = self.oriel.gowave(val, unit)
         self.ui.responsesField.appendPlainText(f"Bytes written: {bts}\n")
         #     delay?
-        time.sleep(round(abs(c_wave-n_wave))/10.0*0.175)
+        time.sleep(math.floor(abs(c_wave-n_wave))/10.0*0.175)
         cw = self.oriel.wave()
         self.ui.waveLabel.setText(WAVE_LABEL_TEXT.format(cw))
 
